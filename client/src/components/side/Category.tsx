ebedb4f5ff5ca49f9c3c9d79c89e9cd108e1a6b8
melonMochiRoll <soulhwasin@gmail.com>
2023-02-25 17:20:21 +0900

    Add Category Component

---
+++ client/src/components/side/Category.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import CategoryItem from 'Components/side/CategoryItem';
+import ListIcon from '@mui/icons-material/FormatListBulleted';
+import { Tag } from 'Components/side/CategoryList';
+
+type CategoryProps = {
+  list: Tag[],
+}
+
+const Category: FC<CategoryProps> = ({ list }) => {
+  return (
+    <Wrapper>
+      <CategoryTitle>
+        <ListIcon fontSize='small' />
+        {list[0].category}
+      </CategoryTitle>
+      {
+        list.map((tag: Tag, i: number) => 
+          <CategoryItem tag={tag} key={i}/>
+        )
+      }
+    </Wrapper>
+  )
+}
+
+export default Category;
+
+const Wrapper = styled.div`
+  margin-bottom: 10px;
+`;
+
+const CategoryTitle = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: #999999;
+  padding: 5px 5px 10px;
+
+  svg {
+    margin-right: 12px;
+  }
+`;
