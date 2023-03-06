308e0d9cf6afa8b1cfe78057b220ae50c3c4a039
melonMochiRoll <soulhwasin@gmail.com>
2023-03-06 20:45:40 +0900

    Modify Props

--- client/src/components/common/MenuButton.tsx
+++ client/src/components/common/MenuButton.tsx
@@ -2,33 +2,38 @@ import React, { FC } from 'react';
 import styled from '@emotion/styled';
 
 type MenuButtonProps = {
+  buttonName: string;
   children: React.ReactNode;
+  color?: string;
 }
 
 const MenuButton: FC<MenuButtonProps> = ({
+  buttonName,
   children,
+  color = '#d9f4d1',
 }) => {
   return (
-    <Button>
+    <Button color={color}>
       {children}
+      {buttonName}
     </Button>
   )
 }
 
 export default MenuButton;
 
-const Button = styled.button`
-  width: 10rem;
-  padding: 0 1.5rem;
-  height: 3rem;
+const Button = styled.button<{color?: string}>`
+  width: 160px;
+  padding: 0.5rem 1.5rem;
+  height: 40px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  font-size: 16px;
+  font-size: 15px;
   font-weight: 500;
   border: none;
   border-radius: 12px;
-  background-color: #d9f4d1;
+  background-color: ${({ color }) => color};
   cursor: pointer;
   gap: 1rem;
 
