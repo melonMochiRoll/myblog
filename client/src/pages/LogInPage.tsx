e6f010014dc7224e34d63906a1fceb42d27a348a
melonMochiRoll <soulhwasin@gmail.com>
2023-03-09 17:07:33 +0900

    Add LogInPage

---
+++ client/src/pages/LogInPage.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import LogInContainer from 'Containers/account/LogInContainer';
+
+const LogInPage: FC = () => {
+  return (
+    <LogInContainer />
+  )
+}
+
+export default LogInPage;
